7b7047aab0e08f49821e2e44fc4bdacb4543daa9
harshalpatil <harshalpatil.er@gmail.com>
2021-11-14 12:25:46 +0530

    Updated form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -20,12 +20,30 @@ export class ReactiveFormComponent implements OnInit {
       rad2: new FormControl(),
       check: new FormControl(),
       rad3: new FormControl() 
-    })
+         })
   }
   ngOnInit(): void {
+ // setValue used when we want to set values for all input controls
+ // we can not omit the single input field 
+ //using patch we can set values for input fields.
+//  this.formLogin.patchValue({
+//    email: 'harshal@angular.com'
+//  })
+   this.formLogin.get('email').statusChanges.subscribe(
+     data => { console.log(data)}
+   )    
+    this.formLogin.statusChanges.subscribe( data=>{ console.log(data)})
 
+   this.formLogin.get('email').valueChanges.subscribe(
+    data => {
+      console.log(data)
+    }
+  )
   }
   onsubmit(){
     console.log(this.formLogin);
+  }
+  resetForm(){
+    this.formLogin.reset();
   }
 }
